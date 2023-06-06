5e3a5643d2e3c0218d5663b4cf8e3495452f4aaa
ridam9795 <ridamnagar11@gmail.com>
2023-06-06 23:28:49 +0530

    Created django model

---
+++ persons_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Person(models.Model):
+    id=models.BigAutoField(primary_key=True)
+    name=models.CharField(max_length=200)
+    email=models.EmailField()
+    age=models.IntegerField(default=0)
